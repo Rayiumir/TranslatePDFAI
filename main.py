03a2b368be9ba3e1d93519378d213448324017d8
Raymond Baghumian <swordart2050@gmail.com>
2025-02-04 04:05:27 +0330

    Add Model - API - Message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import fitz
+import time
+from huggingface_hub import InferenceClient
 
 st.title('Translate PDF')
 st.markdown("""...""", unsafe_allow_html=True)
@@ -11,4 +13,23 @@ if uploaded_file and buttonTranslate:
     pdf = fitz.open(stream=uploaded_file.read(), filetype="pdf")
     for page in range(len(pdf)):
         pages = pdf[page]
-        st.markdown(page.get_text('text'))
+        text = pages.get_text('text')
+        client = InferenceClient(
+            model = "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+            api_key= ""
+        )
+        completion = client.chat.completions.create(
+            messages=[
+                {
+                    'role' : 'system',
+                    'content' : 'translate all texts into fluent Persian'
+                },
+                {
+                    'role' : 'user',
+                    'content' : text
+                }
+            ]
+        )
+
+        st.markdown(completion.choices[0].message.content)
+        time.sleep(3)
